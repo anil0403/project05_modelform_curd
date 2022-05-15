bd097232c69c3fbf011c9f90e3fa5c9e5b71837c
Anil Shrestha <anilshrestha0403.as@gmail.com>
2022-05-15 12:13:24 +0545

    commit

---
+++ project05_modelform_curd/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for project05_modelform_curd project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project05_modelform_curd.settings')
+
+application = get_asgi_application()
