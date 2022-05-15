2686e83b125b21179b070badf89ca91f71e4f0cc
Anil Shrestha <anilshrestha0403.as@gmail.com>
2022-05-15 16:31:10 +0545

    commit

--- curd/urls.py
+++ curd/urls.py
@@ -4,6 +4,4 @@ urlpatterns = [
     path('', views.index, name ='index'),
     path('update/<int:id>/', views.update, name ='update'),
     path('delete/<int:id>/', views.delete, name ='delete'),
-
-
 ]
--- curd/views.py
+++ curd/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import StudentRegistration
 from .models import Student
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def index(request):
@@ -14,18 +15,30 @@ def index(request):
             phone = fm.cleaned_data['phone']
             reg = Student(name=name, email=email, address=address, phone=phone)
             reg.save()
-            fm = StudentRegistration()
+            return HttpResponseRedirect('/')
     else:
         fm = StudentRegistration()
     return render(request, 'index.html', {'form': fm, 'data': data})
 
 
 def update(request,id):
-    return render(request,'update.html')
-
+    fm = StudentRegistration()
+    if request.method == 'POST':
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            name = fm.cleaned_data['name']
+            email = fm.cleaned_data['email']
+            address = fm.cleaned_data['address']
+            phone = fm.cleaned_data['phone']
+            reg = Student(id = id, name = name, email = email, address = address, phone = phone)
+            reg.save()
+            return HttpResponseRedirect('/')
+    else:
+        return render(request,'update.html',{'form':fm})
 
 def delete(request,id):
     delt = Student(id = id)
     delt.delete()
+    return HttpResponseRedirect('/')
   
     
